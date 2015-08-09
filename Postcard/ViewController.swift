c29573abef46681f6d698d46446b3b07af8bfcdb
Alexander Winkler <winkler.alexander@web.de>
2015-08-09 20:05:28 +0200

    nameLabel funcionality

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
-    
+    @IBOutlet weak var nameLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -30,6 +31,12 @@ class ViewController: UIViewController {
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        enterNameTextField.text = ""
         
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
